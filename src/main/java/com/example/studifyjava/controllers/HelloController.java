8704227ced297f662c1be85c2d1eb3573f2b1916
shivam256 <shivamgavandi256@gmail.com>
2022-09-25 17:30:52 +0530

    homepage, add course and add student page

--- src/main/java/com/example/studifyjava/controllers/HelloController.java
+++ src/main/java/com/example/studifyjava/controllers/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.studifyjava.controllers;
 
+import com.example.studifyjava.SceneController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -11,4 +14,15 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    public void handleAddCourseClick() throws IOException {
+        SceneController.launchScene("add-course.fxml");
+    }
+
+    @FXML
+    public void handleAddStudentClick() throws IOException{
+        SceneController.launchScene("add-student.fxml");
+    }
+
 }
